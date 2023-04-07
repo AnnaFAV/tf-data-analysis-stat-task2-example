39c0dded6f23fa94f7e3044e317f377700225e5a
Anna Fedorova <98646915+AnnaFAV@users.noreply.github.com>
2023-04-07 11:28:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,14 +9,14 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     times = 77
-    n = len(х)
-    s = np.mean(х)
-    t_alpha_2 = t.ppf(1 - alpha / 2, n - 1)
+    n = len(x)
+    s = np.mean(x)
+    t_alpha_2 = t.ppf(1 - p / 2, n - 1)
     lambda_ = 1
     scale = s / lambda_
     ci_half_width = t_alpha_2 * scale * np.sqrt((2 * (n - 1)) / (n * (n - 2)))
     a = 2 * s / (times ** 2)
     lbda = 1 / scale
-    left = a - expon.ppf(1 - alpha / 2, scale=1 / lbda) * ci_half_width
-    right = a + expon.ppf(1 - alpha / 2, scale=1 / lbda) * ci_half_width
+    left = a - expon.ppf(1 - p / 2, scale=1 / lbda) * ci_half_width
+    right = a + expon.ppf(1 - p / 2, scale=1 / lbda) * ci_half_width
     return (left, right)
