cd8061fd94c2bc82b433508704d410a02b08223c
Anna Fedorova <98646915+AnnaFAV@users.noreply.github.com>
2023-04-07 11:13:26 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,15 +8,15 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-      t = 77/2
-    s_mean = np.mean(s)
-    s_std = np.std(s, ddof=1)
-    scale = 1/2
-    z = st.norm.ppf((1 + p)/2)
-    interval = z * s_std/np.sqrt(len(x))
-    lower = 2 * s_mean/t**2 - scale * interval
-    upper = 2 * s_mean/t**2 + scale * interval
-    return (lower, upper)
-
-p = 0.95
-x = np.array([30, 35, 25, 28, 32])
+    times = 77
+    n = len(х)
+    s = np.mean(х)
+    t_alpha_2 = t.ppf(1 - alpha / 2, n - 1)
+    lambda_ = 1
+    scale = s / lambda_
+    ci_half_width = t_alpha_2 * scale * np.sqrt((2 * (n - 1)) / (n * (n - 2)))
+    a = 2 * s / (times ** 2)
+    lbda = 1 / scale
+    left = a - expon.ppf(1 - alpha / 2, scale=1 / lbda) * ci_half_width
+    right = a + expon.ppf(1 - alpha / 2, scale=1 / lbda) * ci_half_width
+    return (left, right)
