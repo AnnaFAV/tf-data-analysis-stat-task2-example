c964a43b9cba1374b07bbc886fd37a35a8ddd8f6
Anna Fedorova <98646915+AnnaFAV@users.noreply.github.com>
2023-04-07 11:38:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
+import scipy.stats as st
 
 chat_id = 502755420 # Ваш chat ID, не меняйте название переменной
 
@@ -11,12 +11,13 @@ def solution(p: float, x: np.array) -> tuple:
     times = 77
     n = len(x)
     s = np.mean(x)
-    t_alpha_2 = t.ppf(1 - p / 2, n - 1)
+    t_alpha_2 = st.t.ppf(1 - p / 2, n - 1)
     lambda_ = 1
     scale = s / lambda_
     ci_half_width = t_alpha_2 * scale * np.sqrt((2 * (n - 1)) / (n * (n - 2)))
     a = 2 * s / (times ** 2)
     lbda = 1 / scale
-    left = a - expon.ppf(1 - p / 2, scale=1 / lbda) * ci_half_width
-    right = a + expon.ppf(1 - p / 2, scale=1 / lbda) * ci_half_width
+    left = a - st.expon.ppf(1 - p / 2, scale=1 / lbda) * ci_half_width
+    right = a + st.expon.ppf(1 - p / 2, scale=1 / lbda) * ci_half_width
     return (left, right)
+
